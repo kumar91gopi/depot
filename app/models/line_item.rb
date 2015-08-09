4c2e728c70da0e90c059841824a7fa23d39e5cd6
kumar91gopi <kumar91gopi@gmail.com>
2015-08-09 23:49:44 +0000

    commit

---
+++ app/models/line_item.rb
@@ -0,0 +1,8 @@
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+  has_many :line_items, dependent: :destroy
+  def total_price
+    product.price * quantity
+  end
+end
